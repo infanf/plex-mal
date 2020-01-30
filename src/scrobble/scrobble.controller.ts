573b614b95d08cb3523b2366bfa6926e0f55a55c
Gitea <gitea@fake.local>
2020-01-30 17:05:31 +0100

    fix: read data

--- src/scrobble/scrobble.controller.ts
+++ src/scrobble/scrobble.controller.ts
@@ -1,4 +1,7 @@
-import { Controller, Param, Post, UploadedFile } from '@nestjs/common';
+import { Controller, Param, Post, Request } from '@nestjs/common';
+import * as Busboy from 'busboy';
+import { Request as Req } from 'express';
+import { inspect } from 'util';
 
 import { ScrobbleService } from './scrobble.service';
 
@@ -6,7 +9,11 @@ import { ScrobbleService } from './scrobble.service';
 export class ScrobbleController {
   constructor(private readonly scrobbleService: ScrobbleService) {}
   @Post(':authtoken')
-  async scrobble(@Param('authtoken') token: string, @UploadedFile() data: unknown) {
-    this.scrobbleService.scrobble(data);
+  async scrobble(@Param('authtoken') token: string, @Request() req: Req) {
+    const busboy = new Busboy({ headers: req.headers });
+    busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
+      this.scrobbleService.scrobble(inspect(val));
+    });
+    req.pipe(busboy);
   }
 }
